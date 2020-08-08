f50cad1a28f8f2e094dfdcc8bf08012cdb754ff6
Neha Chauhan <64642273+nehachauhan22@users.noreply.github.com>
2020-08-08 15:40:06 +0530

    Update server.py
    Binding Socket and Connections

--- server.py
+++ server.py
@@ -10,3 +10,17 @@ def create_socket():
 		s = socket.socket()
 	except socket.error as msg:
 		print("socket creation error "+str(msg))
+#binding the socket and listening for connections 
+def bind_socket():
+	try:
+		global host
+		global s
+		global port		
+		print("binding the port "+str(port))
+		s.bind((host,port))#tuple to bind the host and the socket 
+		s.listen(5)
+		#here 5 is the no of bad connections it will tolorate
+
+	except socket.error as msg:
+		print("socket binding error "+str(msg)+"\n retrying")
+		bind_socket()
